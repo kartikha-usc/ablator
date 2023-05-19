c5451e100a5221ff3e69683ef90b32baf51045cc
kartikha-usc <122422618+kartikha-usc@users.noreply.github.com>
2023-05-18 19:24:26 -0700

    Update test_albator_model.py
    added one more test case

--- tests/modules/test_albator_model.py
+++ tests/modules/test_albator_model.py
@@ -18,6 +18,34 @@ class AblateModelTest(unittest.TestCase):
         # Try to ablate the model
         with self.assertRaises(ValueError):
             ablate_model(model, ablation_strategy)
+      
+    def test_ablate_model(self):
+        # Create a model
+        model = torch.nn.Sequential(
+            torch.nn.Linear(10, 10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10, 10)
+        )
+
+        # Define an ablation strategy
+        ablation_strategy = [
+            "remove_layer",
+            "remove_neurons"
+        ]
+
+        # Ablate the model
+        ablated_model = ablate_model(model, ablation_strategy)
+
+        # Check that the ablated model has the correct number of layers
+        self.assertEqual(len(ablated_model), len(model) - 1)
+
+        # Check that the ablated model has the correct number of neurons
+        self.assertEqual(ablated_model[0].out_features, model[0].out_features - 1)
+
+        # Check that the ablated model can still be used
+        ablated_model(torch.rand(10))
+
+
 
 def main():
     unittest.main()
